4eb31fd94df67563241a35872a9dc6024b1d8e02
Ingo Bernhardt <bernhardt@exzentriker.it>
2019-12-23 09:20:02 +0100

    removed i18n for ContentEntity

--- Kofus/System/src/Kofus/System/Entity/ContentEntity.php
+++ Kofus/System/src/Kofus/System/Entity/ContentEntity.php
@@ -11,7 +11,7 @@ use Kofus\System\Node;
  * @ORM\Table(name="kofus_system_contents")
  *
  */
-class ContentEntity implements Node\NodeInterface, Node\I18nNodeInterface, Node\NodeModifiedInterface, Node\ParameterInterface
+class ContentEntity implements Node\NodeInterface, Node\NodeModifiedInterface, Node\ParameterInterface
 {
     
 
@@ -69,35 +69,7 @@ class ContentEntity implements Node\NodeInterface, Node\I18nNodeInterface, Node\
 		return $this->content;
 	}
 	
-	/**
-	 * @ORM\Column(length=5, nullable=true)
-	 */
-	protected $locale;
 	
-	public function getLocale()
-	{
-	    return $this->locale;
-	}
-	
-	public function setLocale($value)
-	{
-	    $this->locale = $value; return $this;
-	}
-	
-	/**
-	 * @ORM\Column(nullable=true)
-	 */
-	protected $i18nId;
-	
-	public function getI18nId()
-	{
-	    return $this->i18nId;
-	}
-	
-	public function setI18nId($value)
-	{
-	    $this->i18nId = $value; return $this;
-	}
 	
 	/**
 	 * @ORM\Column(type="boolean")
