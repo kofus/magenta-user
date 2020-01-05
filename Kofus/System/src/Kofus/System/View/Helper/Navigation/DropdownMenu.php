5547979f4e657402507ad417034089f480662bff
Ingo Bernhardt <bernhardt@exzentriker.it>
2020-01-05 13:51:08 +0100

    support navigation divider

--- Kofus/System/src/Kofus/System/View/Helper/Navigation/DropdownMenu.php
+++ Kofus/System/src/Kofus/System/View/Helper/Navigation/DropdownMenu.php
@@ -227,8 +227,10 @@ class DropdownMenu extends \Zend\View\Helper\Navigation\Menu
             }
             $liClass = empty($liClasses) ? '' : ' class="' . $escaper(implode(' ', $liClasses)) . '"';
 
-            $html .= $myIndent . '    <li' . $liClass . '>' . PHP_EOL
-                . $myIndent . '        ' . $this->htmlify($page, $escapeLabels, $addClassToListItem) . PHP_EOL;
+            $html .= $myIndent . '    <li' . $liClass . '>'  . PHP_EOL
+                . $myIndent . '        ';
+            if (! in_array('divider', $liClasses))
+                $html .= $this->htmlify($page, $escapeLabels, $addClassToListItem) . PHP_EOL;
 
             // store as previous depth for next iteration
             $prevDepth = $this->depth;
