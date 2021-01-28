0289f1e5f0a5e6c9cd43a2b190422c325eab8fe4
Ingo Bernhardt <bernhardt@exzentriker.it>
2021-01-28 19:09:41 +0100

    bug fix paginator coun

--- Kofus/System/src/Kofus/System/Paginator/Adapter/NodeAdapter.php
+++ Kofus/System/src/Kofus/System/Paginator/Adapter/NodeAdapter.php
@@ -1,6 +1,9 @@
 <?php
 namespace Kofus\System\Paginator\Adapter;
 use Zend\Paginator\Adapter\AdapterInterface;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\CountWalker;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class NodeAdapter implements AdapterInterface
 {
@@ -20,15 +23,32 @@ class NodeAdapter implements AdapterInterface
         return $qb->getQuery()->getResult();
     }
     
+    protected $count;
+    
     public function count()
     {
-        $sql = $this->qb->getQuery()->getSql();
-        $conn = $this->qb->getEntityManager()->getConnection();
-        $stmt = $conn->prepare('SELECT COUNT(*) AS c FROM ('.$sql.') AS q');
-        $stmt->execute();
-        
-        $results = $stmt->fetchAll();
-        return $results[0]['c']; 
+        if (null === $this->count) {
+            $countQuery = $this->qb->getQuery();
+            $countQuery->setHint(CountWalker::HINT_DISTINCT, true);
+            
+            $platform = $countQuery->getEntityManager()->getConnection()->getDatabasePlatform(); // law of demeter win
+            
+            $rsm = new ResultSetMapping();
+            $rsm->addScalarResult($platform->getSQLResultCasing('dctrn_count'), 'count');
+            
+            $countQuery->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, 'Doctrine\ORM\Tools\Pagination\CountOutputWalker');
+            $countQuery->setResultSetMapping($rsm);
+            $countQuery->setFirstResult(null)->setMaxResults(null);
+            try {
+                $data =  $countQuery->getScalarResult();
+                $data = array_map('current', $data);
+                $this->count = array_sum($data);
+            } catch(\Exception $e) {
+                $this->count = 0;
+            }
+        }
+            
+        return $this->count;
     }
     
 }
